0be00243e6b89da56e89f52ca2761b831c330a83
Ríad Oliveira <riad.oliveira@hotmail.com>
2020-10-22 03:09:27 -0300

    Update BankData.js

--- public/scripts/BankData.js
+++ public/scripts/BankData.js
@@ -15,7 +15,7 @@ function verifyDate () {
             } else {
                 submitButton.type = 'submit'
             }
-
+            
         } else {
             submitButton.removeEventListener('click', verifyDate)
         }
